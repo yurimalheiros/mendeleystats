c3bd061157a4b88c549f154148b845f15465cde6
yurimalheiros <yurimalheiros@gmail.com>
2013-07-10 15:34:27 -0300

    habemus mendeley stats

---
+++ README.md
@@ -0,0 +1,50 @@
+Mendeley Stats
+##############
+
+A command line tool to extract information about papers in a Mendeley folder.
+
+## Installation
+
+### OS X and Linux:
+
+```
+pip install mendeleystats
+```
+
+Also, you need to set API keys to a configuration file. First, enter http://dev.mendeley.com/ and register an app.
+to access the values of Consumer Key and Consumer Secret. With this information in hands, create a
+file .mendeleystatsconfig in your home directory. The content of this file must follow the format:
+
+```
+{
+    "api_key" : "the value of the consumer key",
+    "api_secret" : "the value of the consumer secret"
+}
+```
+
+### Windows
+
+I do not know. I accept help to write this :)
+
+
+## How to use
+
+It is super simple to use:
+
+```
+mendeleystats [-h] [--info {year,type,authors,keywords,publishedin}]
+                [--output {chart,csv}] [--folder FOLDER]
+```
+
+For example:
+
+```
+mendeleystats --info year --output chart --folder datamining
+```
+
+It displays a chart with information about papers per year from the "datamining" folder.
+You can get other types of information, you only need to change the value of the argument --info,
+the options are: year, type, authors, keywords, and publishedin.
+
+Besides, you can choose to save a .csv file with the information instead get a chart.
+For this, you need to use the value csv in the argument --output.
---
+++ stats.py
@@ -0,0 +1,209 @@
+import os
+import sys
+import csv
+import itertools
+import argparse
+from pylab import *
+import numpy.numarray as na
+from mendeley_client import *
+from collections import Counter
+
+class Folder(object):
+    def __init__(self, config_file):
+        self.folder_id = None
+        self.documents = []
+        self.years_counter = None
+        self.types_counter = None
+        self.keywords_counter = None
+        self.authors_counter = None
+        self.published_in_counter = None
+
+        self.mendeley = create_client(config_file)
+
+    ########## Public methods. ##########
+
+    def load_data(self, folder_name):
+        """
+        Load folder data from the Mendeley server.
+
+        Args:
+            folder_name: the name of the folder.
+        """
+        folders = self.mendeley.folders()
+
+        for folder in folders:
+            if folder["name"] == folder_name:
+                self.load_data_by_id(folder["id"])
+
+    def load_data_by_id(self, folder_id):
+        """
+        Load folder data from the Mendeley server.
+
+        Args:
+            folder_id: the id of the folder.
+        """
+        documents_request = self.mendeley.folder_documents(folder_id, items=999)
+        documents = documents_request["documents"]
+        self.documents = [self.mendeley.document_details(d["id"]) for d in documents]
+
+        self.years_counter = self._get_years_counter()
+        self.types_counter = self._get_types_counter()
+        self.keywords_counter = self._get_keywords_counter()
+        self.authors_counter = self._get_authors_counter()
+        self.published_in_counter = self._get_published_in_counter()
+
+    def plot_years(self):
+        """Show a chart with papers per year."""
+        self._plot(self.years_counter)
+
+    def plot_types(self):
+        """Show a chart with papers per publication type."""
+        self._plot(self.types_counter)
+
+    def plot_keywords(self):
+        """Show a chart with papers per keyword."""
+        self._plot(self.keywords_counter)
+
+    def plot_authors(self):
+        """Show a chart with papers per author."""
+        self._plot(self.authors_counter)
+
+    def plot_published_in(self):
+        """Show a chart with papers per published outlet."""
+        self._plot(self.published_in_counter)
+
+    def csv_years(self):
+        """Create a .csv file with papers per year data."""
+        self._save_csv(self.years_counter, "years.csv")
+
+    def csv_types(self):
+        """Create a .csv file with papers per publication type data."""
+        self._save_csv(self.types_counter, "types.csv")
+
+    def csv_keywords(self):
+        """Create a .csv file with papers per keyword data."""
+        self._save_csv(self.keywords_counter, "keywords.csv")
+
+    def csv_authors(self):
+        """Create a .csv file with papers per author data."""
+        self._save_csv(self.authors_counter, "authors.csv")
+
+    def csv_published_in(self):
+        """Create a .csv file with papers per published outlet data."""
+        self._save_csv(self.published_in_counter, "published.csv")
+
+
+    ########## Private methods. ##########
+
+    def _get_years_counter(self):
+        """Return a Counter of papers per year."""
+        year = []
+        for d in self.documents:
+            year.append(d.get("year", "not defined")) 
+
+        return Counter(year)
+
+    def _get_types_counter(self):
+        """Return a Counter of papers per publication type."""
+        type = []
+        for d in self.documents:
+            type.append(d.get("type", "not defined")) 
+
+        return Counter(type)
+
+    def _get_keywords_counter(self):
+        """Return a Counter of papers per keyword."""
+        keywords = [d["keywords"] for d in self.documents]
+        keywords = list(itertools.chain.from_iterable(keywords))
+
+        keywords_aux = []
+        for k in keywords:
+            keywords_aux.extend(k.split(";"))
+
+        keywords = keywords_aux
+        keywords = [k.strip() for k in keywords]
+        keywords = filter(lambda x: x!="", keywords)
+
+        return Counter(keywords)
+
+    def _get_authors_counter(self):
+        """Return a Counter of papers per author."""
+        authors = [d["authors"] for d in self.documents]
+        authors_fullname = []
+        for author_list in authors:
+            for author in author_list:
+                authors_fullname.append(author["forename"] + " " + author["surname"])
+
+        return Counter(authors_fullname)
+
+    def _get_published_in_counter(self):
+        """Return a Counter of papers per publish outlet."""
+        published = []
+        for d in self.documents:
+            published.append(d.get("published_in", "not defined")) 
+
+        return Counter(published)
+        
+        
+    def _plot(self, counter, vertical=False):
+        """Create a chart with a Counter data."""
+        labels = counter.keys()
+        labels.sort()
+        data = [counter[l] for l in labels]
+        
+        xlocations = na.array(range(len(data)))+0.5
+        width = 0.5
+
+        bar(xlocations, data, width=width)
+        tlocs, tlabels = xticks(xlocations+width/2, labels)
+        xlim(0, xlocations[-1]+width*2)
+        title("Papers")
+        gca().get_xaxis().tick_bottom()
+        gca().get_yaxis().tick_left()
+        
+        if vertical:
+            setp(tlabels, "rotation", "vertical")
+
+        show()
+
+    def _save_csv(self, counter, filename):
+        """Create a .csv file with a Counter data."""
+        with open(filename, 'w') as fp:
+            writer = csv.writer(fp, delimiter=',')
+            writer.writerows(counter.items())
+
+
+########## Main ##########
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Mendeley Stats")
+    parser.add_argument("--info", choices=["year", "type", "authors", "keywords", "publishedin"], help='paper information')
+    parser.add_argument("--output", choices=["chart", "csv"], help='data presentation type')
+    parser.add_argument("--folder", help='folder name')
+    args = parser.parse_args()
+
+    config_path = os.path.join(os.path.expanduser("~"), ".mendeleystatsconfig")
+    if not os.path.exists(config_path):
+        print "I can't run."
+        print "Please, create a .mendeleystatsconfig file with the API keys in your home directory."
+        sys.exit(0)
+
+
+    folder = Folder(config_path)
+    folder.load_data(args.folder)
+
+    actions = {
+        "year chart" : folder.plot_years,
+        "type chart" : folder.plot_types,
+        "authors chart" : folder.plot_authors,
+        "keywords chart" : folder.plot_keywords,
+        "publishedin chart" : folder.plot_published_in,
+        "year csv" : folder.csv_years,
+        "type csv" : folder.csv_types,
+        "authors csv" : folder.csv_authors,
+        "keywords csv" : folder.csv_keywords,
+        "publishedin csv" : folder.csv_published_in,
+    }
+
+    actions[args.info + " " + args.output]()
+
